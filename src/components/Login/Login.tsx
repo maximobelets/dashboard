6a48a735ff57bb0bdbdc1fb46c6ab886748d178a
maximobelets <maximobelets@gmail.com>
2023-02-04 15:59:44 +0500

    Add form for login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,37 @@
+import React from "react";
+
 export const Login = () => {
+    const [login, setLogin] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+
+    const formSubmit = (e: React.FormEvent) => {
+        e.preventDefault;
+        console.log('Form submit')
+    }
+
     return (
         <>
             <h1>Login</h1>
+            <form onSubmit={formSubmit}>
+                <label>
+                    <input
+                        value={login}
+                        type="text"
+                        onChange={(e: React.FormEvent<HTMLInputElement>) => setLogin(e.currentTarget.value)}
+                    />
+                </label>
+                <label>
+                    <input
+                        value={password}
+                        type="password"
+                        onChange={(e: React.FormEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)}
+                    />
+                </label>
+                <button type="submit">
+                    s
+                </button>
+            </form>
         </>
     )
 }
