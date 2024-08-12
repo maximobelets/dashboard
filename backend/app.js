01cd9cc179147188678293a9f478ec087fc2b4d7
maximobelets <maximobelets@gmail.com>
2024-08-12 17:55:43 +0500

    Add generateToken function

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,18 @@ const app = express();
 
 const port = process.env.PORT;
 
+const generateToken = (user) => {
+    const payload = {
+        id: user.id,
+        email: user.email
+    };
+      
+    const secret = process.env.SECRET_KEY;
+    const options = { expiresIn: '1d' };
+    
+    return jwt.sign(payload, secret, options);
+}
+
 app.get('/', (req, res) => {
     res.send('App');
 })
