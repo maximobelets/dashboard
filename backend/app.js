a73c8e47f85122d058e31cb766ddb8506c2608e2
maximobelets <maximobelets@gmail.com>
2024-08-14 17:55:21 +0500

    Add post method for jwt creation

--- backend/app.js
+++ backend/app.js
@@ -12,12 +12,18 @@ const generateToken = (user) => {
         id: user.id,
         email: user.email
     };
-      
+
     const secret = process.env.SECRET_KEY;
     const options = { expiresIn: '1d' };
-    
+
     return jwt.sign(payload, secret, options);
 }
+
+app.post('/user/newToken', (req, res) => {
+    const newToken = generateToken({id: 1, email: 'test@test.test'})
+
+    res.send(newToken)
+})
 
 app.get('/', (req, res) => {
     res.send('App');
